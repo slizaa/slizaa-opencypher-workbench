416698d442f7f62cef410a386c52595233d59ddc
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-12-05 16:53:33 +0100

    ~ further development

--- plugins/org.slizaa.neo4j.queryresult.ui/web-src/components/QueryResultComponent.tsx
+++ plugins/org.slizaa.neo4j.queryresult.ui/web-src/components/QueryResultComponent.tsx
@@ -1,12 +1,84 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as styles from "./queryResultComponent.css";
+import 'react-virtualized/styles.css'; // only needs to be imported once
+import { SortDirection, AutoSizer, Table, Column, List } from "react-virtualized"
 
 export interface QueryResultComponentProps { columnNames: string[]; records: string[]; }
 
 export class QueryResultComponent extends React.PureComponent<QueryResultComponentProps, {}> {
-    
+
+    private heightCache: Map<string, number>;
+
+    // the constructor
+    constructor(props: QueryResultComponentProps) {
+        super(props);
+
+        // 
+        this.heightCache = new Map();
+    }
+
     render() {
-        return <h1 className={styles.noRows}>Hello from {this.props.columnNames.length} and {this.props.records.length}!</h1>;
+
+        const { columnNames, records } = this.props;
+
+        return (
+
+            <Table
+                className={styles.tableHeader}
+                width={600}
+                height={600}
+                headerHeight={20}
+                estimatedRowSize={180}
+                rowHeight={this.computeHeight}
+                rowCount={records.length}
+                rowGetter={({ index }) => records[index]}
+                noRowsRenderer={this.noRowsRenderer}
+            >
+                {columnNames.map((name) => <Column className={styles.tableColumn} key={name} width={this.computeWidth()} label={name} dataKey={name} cellDataGetter={this.getCellData} cellRenderer={({ cellData }) => this.renderCell(cellData)} />)}
+            </Table>
+
+        );
+    }
+
+    noRowsRenderer() {
+        return <div className={styles.noRows}>No Rows</div>
+    }
+
+    computeWidth(): number {
+        return 50;
+    }
+
+    computeHeight({ index }: any): number {
+        return 200;
+    }
+
+    getCellData({
+        columnData,
+        dataKey,
+        rowData
+        }: any) {
+        return rowData[dataKey];
+    }
+
+    renderCell(cellData: any) {
+
+        console.log("renderCell")
+
+        //
+        if (typeof cellData === 'object') {
+            return <table className={styles.nodeTable}>
+                <tbody>
+                    <tr><td>ID</td><td>{cellData.id}</td></tr>
+                    <tr><td>Labels</td><td>{"[" + cellData.labels + "]"}</td></tr>
+                    {Object.keys(cellData.properties).map(key => <tr><td>{key}</td><td>{"" + cellData.properties[key]}</td></tr>)}
+                </tbody>
+            </table>
+        }
+
+        //
+        else {
+            return <div className={styles.attributeValue}>{cellData}</div>
+        }
     }
 }
--- plugins/org.slizaa.neo4j.queryresult.ui/web-src/components/queryResultComponent.css.d.ts
+++ plugins/org.slizaa.neo4j.queryresult.ui/web-src/components/queryResultComponent.css.d.ts
@@ -1,4 +1,6 @@
 export const tableSpace: string;
+export const tableHeader: string;
+export const tableColumn: string;
 export const nodeTable: string;
 export const attributeValue: string;
 export const noRows: string;
